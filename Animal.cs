2d5b5cc8256cc3c33e3b54dee2b91867e628790b
Tuuli <tuuli.annus@centria.fi>
2021-05-21 15:38:41 +0300

    create class Animal and list of animals

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+namespace Assignment_centria
+{
+    public class Animal
+    {
+        public string species; 
+
+        public string status;
+        public int weight;
+        public string name; 
+
+        public Animal(string species, string status, int weight, string name)
+    
+        {
+
+            this.species = species; 
+            this.status = status;
+            this.weight = weight;
+            this.name = name; 
+            
+
+        }
+
+    }
+}
