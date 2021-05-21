594d3c2f51fe4d6bac036a9cdb01b0ee660de8b3
Tuuli <tuuli.annus@centria.fi>
2021-05-21 16:11:41 +0300

    add reading function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Assignment_centria
 {
-    class Program
+    internal class NewBaseType
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Animal> animals = new List<Animal>();
+
+            string[] lines = File.ReadAllLines("animals.csv");
+            foreach (string line in lines)
+            {
+                string[] pieces = line.Split(",");
+                string v = pieces[0];
+                string animal = v;
+                Console.WriteLine(animal);
+            }
         }
     }
+
+  
 }
